73ecc2c11c56801eebb29627227fb20aadd406b5
494917128 <494917128@qq.com>
2019-09-24 17:32:11 +0800

    1

--- src/js/Config.ts
+++ src/js/Config.ts
@@ -2,6 +2,6 @@
 常量设置
 ***************/
 export enum Config {
-  STORAGE_HEAD = 'LFC_', // 设置缓存的开头
-  AXIOS_HOST = 'https://www.liveforcool.com/api/', // axios请求的域名
+  STORAGE_HEAD = 'example_', // 设置缓存的开头
+  AXIOS_HOST = 'https://www.example.com/api/', // axios请求的域名
 }
